078419ce1e6e7a5c8b33828759a465076e99185b
Saviru <savirukashmira@gmail.com>
2026-03-07 17:26:15 +0530

    Update global layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,36 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+SplashScreen.preventAutoHideAsync();
+
+export { ErrorBoundary } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  const hasLoadedAssets = true;
+
+  useEffect(() => {
+    if (hasLoadedAssets) {
+      SplashScreen.hideAsync();
+    }
+  }, [hasLoadedAssets]);
+
+  if (!hasLoadedAssets) {
+    return null;
+  }
+
+  return (
+    <SafeAreaProvider>
+      <Stack
+        screenOptions={{
+          // Global header
+          headerShown: false,
+          animation: "slide_from_right",
+        }}
+      >
+        <Stack.Screen name="index" />
+      </Stack>
+    </SafeAreaProvider>
+  );
 }
