f4c78d64bc782b49a539d51f469d239ac45893ed
Saviru <savirukashmira@gmail.com>
2026-03-25 22:47:41 +0530

    Update StatCard ui element

--- packages/ui/components/ui/StatCard/StatCard.types.ts
+++ packages/ui/components/ui/StatCard/StatCard.types.ts
@@ -3,8 +3,9 @@ import { ViewStyle, StyleProp } from 'react-native';
 
 export interface StatCardProps {
   label: string;
-  value: string | number;
-  icon?: React.ReactNode | string;
+  value: string | number | React.ReactNode;
+  subtitle?: string | React.ReactNode;
+  icon?: string | React.ReactNode;
   accentColor?: string;
   style?: StyleProp<ViewStyle>;
 }
