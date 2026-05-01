8d23d58248b49daf985e39d994f7fc61acd31b08
Saviru <savirukashmira@gmail.com>
2026-05-01 21:09:08 +0530

    Fix: Add path to jest config

--- jest.config.js
+++ jest.config.js
@@ -7,12 +7,14 @@ module.exports = {
     "/node_modules/",
     "/dist/",
     "/gt/",
+    "/tt/",
     "/p1/",
     "/proj3/",
     "/proj4/",
     "/poj2/",
     "/Proj5/",
   ],
+  modulePathIgnorePatterns: ["<rootDir>/gt/", "<rootDir>/tt/", "<rootDir>/p1/", "<rootDir>/proj3/", "<rootDir>/proj4/", "<rootDir>/poj2/", "<rootDir>/Proj5/"],
   moduleNameMapper: {
     // Redirect all ThemeContext imports to the manual mock
     "(.*)context/ThemeContext": "<rootDir>/packages/ui/context/__mocks__/ThemeContext.tsx",
