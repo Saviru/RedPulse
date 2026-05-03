0c56fd7fbd7378979db61982703c8dea99a0ee23
AmashiJayawardhana <amashithathsarani025@gmail.com>
2026-05-03 18:07:35 +0530

    Revert "update"
    This reverts commit 6eb4818687b8609489b05f344544de47e37558e1.

--- api/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/src/tests/setup.ts'],
-  testMatch: ['**/*.test.ts'],
-  clearMocks: true,
-};
